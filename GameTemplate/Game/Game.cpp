d211c6a2937870e82e82da96cae52bdf983a2fa0
takuya yamamoto <takuyahappiness404@gmail.com>
2020-01-21 11:55:36 +0900

    ブレンドステートの更新

--- GameTemplate/Game/Game.cpp
+++ GameTemplate/Game/Game.cpp
@@ -12,25 +12,24 @@ Game* g_game = nullptr;
 
 Game::Game()
 {
-	g_game = this;
-	m_sprite.Init(L"Assets/Sprite/robot.dds",1200, 1200);
+	m_sprite.Init(L"Assets/Sprite/unity.dds",10000, 10000);
 	m_soundEngine.Init();
 
 	//m_bgm.Init(L"Assets/sound/noroinouta.wav");
 	//m_bgm.Play(true);
 
-	////レベルを初期化。
-	//m_level.Init(L"Assets/level/School.tkl", [&](LevelObjectData& objData) {
-	//	if (objData.EqualName(L"enemy") == true) {
-	//		//Star。
-	//		auto enemy = new Enemy(objData.position, objData.rotation, &m_player);
-	//		enemyList.push_back(enemy);
-	//		return true;
-	//	}
-	//	return false;
-	//});
+	//レベルを初期化。
+	m_level.Init(L"Assets/level/School.tkl", [&](LevelObjectData& objData) {
+		if (objData.EqualName(L"enemy") == true) {
+			//Star。
+			auto enemy = new Enemy(objData.position, objData.rotation, &m_player);
+			enemyList.push_back(enemy);
+			return true;
+		}
+		return false;
+	});
 
-	//m_player.SetPosition({ -400.0f,0.0f,800.0f });
+	m_player.SetPosition({ -400.0f,0.0f,800.0f });
 
 	InitTranslucentBlendState();
 	m_camera.SetPlayer(&m_player);
@@ -40,9 +39,6 @@ Game::Game()
 
 Game::~Game()
 {
-
-	g_game = nullptr;
-
 	for (auto& enemy : enemyList) {
 		delete enemy;
 	}
@@ -53,7 +49,7 @@ Game::~Game()
 
 void Game::InitTranslucentBlendState()
 {
-	//例のごとく、作成するブレンドステートの情報を設定する。
+	//作成するブレンドステートの情報を設定する。
 	CD3D11_DEFAULT defaultSettings;
 	//デフォルトセッティングで初期化する。
 	CD3D11_BLEND_DESC blendDesc(defaultSettings);
@@ -65,7 +61,7 @@ void Game::InitTranslucentBlendState()
 	//この指定では、ソースカラーをSRC(rgba)とすると、
 	//最終的なソースカラーは下記のように計算される。
 	//最終的なソースカラー　= SRC.rgb X SRC.a
-	blendDesc.RenderTarget[0].SrcBlend = D3D11_BLEND_ONE;
+	blendDesc.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA;
 
 	//ディスティネーションカラーのブレンディング方法を指定している。
 	//ディスティネーションカラーとは、
@@ -74,7 +70,7 @@ void Game::InitTranslucentBlendState()
 	//ソースカラーをSRC(RGBA)とすると、最終的なディスティネーションカラーは
 	//下記のように計算される
 	//最終的なディスティネーションカラー = DEST.rgb X (1.0f - SRC.a)
-	blendDesc.RenderTarget[0].DestBlend = D3D11_BLEND_ONE;
+	blendDesc.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_ALPHA;
 
 	//最終的にレンダリングターゲットに描きこまれるカラーの計算方法を指定している
 	//この指定だと、1＋2のカラーが描きこまれる
@@ -93,11 +89,15 @@ void Game::Update()
 {
 	//プレイヤーの更新。
 	m_player.Update();
+	//エネミーの更新
 	for (auto& enemy : enemyList) {
 		enemy->Update();
 	}
+	//サウンドの更新
 	m_soundEngine.Update();
+	//カメラの更新
 	m_camera.Update();
+	//画像の更新
 	m_sprite.UpdateWorldMatrix(CVector3::Zero(), CQuaternion::Identity(), CVector3::One());
 }
 
@@ -120,8 +120,7 @@ void Game::Render()
 		{ 0,1,0 }
 	);
 	mProj.MakeOrthoProjectionMatrix(1280, 720, 0.1, 100);
-	m_sprite.Draw(mView, mProj);
-	
+
 	float blendFactor[] = { 0.0f,0.0f,0.0f,0.0f };
 	//半透明合成もブレンドステートを設定する。
 	d3dDeviceContext->OMSetBlendState(
@@ -129,13 +128,14 @@ void Game::Render()
 		blendFactor,
 		0xffffffff
 	);
-
-	////プレイヤーの描画。
-	//m_player.Draw();
-	////レベルを描画
-	//m_level.Draw();
-	////エネミーを描画
-	//for (auto& enemy : enemyList) {
-	//	enemy->Draw();
-	//}
+	m_sprite.Draw(mView, mProj);
+	
+	//プレイヤーの描画。
+	m_player.Draw();
+	//レベルを描画
+	m_level.Draw();
+	//エネミーを描画
+	for (auto& enemy : enemyList) {
+		enemy->Draw();
+	}
 }
